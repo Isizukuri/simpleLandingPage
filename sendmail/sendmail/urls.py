f99e3352e0822d66f4aee827e328e70c0053759b
Isizukuri <ragnarok49@gmail.com>
2015-12-08 17:47:27 +0200

    Project init

---
+++ sendmail/sendmail/urls.py
@@ -0,0 +1,27 @@
+"""sendmail URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from django.views.generic.base import View
+
+from simpleapp.views import send_mail
+
+urlpatterns = [
+	url(r'^$', View.as_view(), name='home'),
+	url(r'^send_email/', send_mail),
+    url(r'^admin/', admin.site.urls),
+]
---
+++ sendmail/simpleapp/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponseRedirect
+
+# Create your views here.
+
+def send_mail(request):
+	print 'Success'
+	return HttpResponseRedirect()
+
