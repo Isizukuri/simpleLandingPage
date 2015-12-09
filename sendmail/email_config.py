0e28726771bb8ed8e40845e487309f2e1ef58f30
progand <lach@ukr.net>
2015-12-09 17:51:31 +0200

    [*] hiden email config file

--- sendmail/email_config.py
+++ sendmail/email_config.py
@@ -1,7 +1,7 @@
-destination_email = 'ragnarok49@gmail.com'
-smtp_name = 'smtp.gmail.com'
-smtp_port = '465'
-smtp_user = 'ragnarok49@gmail.com'
-smtp_pass = 'pass'
+destination_email = 'al@livarava.com'
+smtp_name = 'smtp.mandrillapp.com'
+smtp_port = '587'
+smtp_user = 'artem@kariavka.com'
+smtp_pass = 'OIVvaPEAJLtK2jLCB76MiQ'
 use_tls = False
-use_ssl = True
+use_ssl = False
