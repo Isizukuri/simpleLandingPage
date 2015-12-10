07b9a8391645786c39af06086382e1696120277d
progand <lach@ukr.net>
2015-12-10 16:06:59 +0200

    [*] layout

--- sendmail/simpleapp/static/js/form.js
+++ sendmail/simpleapp/static/js/form.js
@@ -1,3 +1,16 @@
-/**
- * Created by user on 12/10/15.
- */
+angular.module('LRLandingPage', ['reCAPTCHA'])
+    .config(function (reCAPTCHAProvider) {
+        // required: please use your own key :)
+        reCAPTCHAProvider.setPublicKey('---KEY---');
+
+        // optional: gets passed into the Recaptcha.create call
+        reCAPTCHAProvider.setOptions({
+            theme: 'clean'
+        });
+    })
+    .controller('AppCtrl', function ($scope, reCAPTCHA) {
+
+        // or you can also set key here
+        reCAPTCHA.setPublicKey('---KEY---');
+
+    });
