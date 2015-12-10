ef9922c1fde55df4cbd4b35991ef81fba9815009
Isizukuri <Isizukuri@users.noreply.github.com>
2015-12-10 15:46:03 +0200

    Delete email_config.py

--- sendmail/email_config.py
+++
@@ -1,7 +0,0 @@
-destination_email = 'al@livarava.com'
-smtp_name = 'smtp.mandrillapp.com'
-smtp_port = '587'
-smtp_user = 'artem@kariavka.com'
-smtp_pass = 'OIVvaPEAJLtK2jLCB76MiQ'
-use_tls = False
-use_ssl = False
