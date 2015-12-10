fc2fb6b1bd10520982065364d039d216d1b743f3
Isizukuri <ragnarok49@gmail.com>
2015-12-10 09:48:50 +0200

    Added form processing by MainPage view

--- sendmail/simpleapp/tests.py
+++ sendmail/simpleapp/tests.py
@@ -2,6 +2,40 @@ from django.test import TestCase, Client
 from django.core import mail
 from django.core.urlresolvers import reverse
 # Create your tests here.
+class TestMainPageView(TestCase):
+
+
+    def setUp(self):
+        
+        self.client = Client()
+
+        self.url = reverse('home')
+
+    def test_get_mainpage(self):
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertTemplateUsed(response, 'home.html')
+    
+    def test_send_email(self):
+        response = self.client.post(self.url, {'name': 'Test Name',
+            'category': 'Test Category', 'subject': 'Test Subject',
+            'message': 'Test Message', 'captcha': 'test cap'}, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+        self.assertEqual(len(mail.outbox), 1)
+
+        self.assertEqual(mail.outbox[0].subject, 'test message')
+
+
+    def test_redirect(self):
+        
+        response = self.client.post(self.url, {'name': 'Test Name',
+            'category': 'Test Category', 'subject': 'Test Subject',
+            'message': 'Test Message', 'captcha': 'test cap'}, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+
+        self.assertRedirects(response, reverse('home'), status_code=302, 
+            target_status_code=200, msg_prefix='', fetch_redirect_response=True)
 
 
 class TestSendMailView(TestCase):
--- sendmail/simpleapp/views.py
+++ sendmail/simpleapp/views.py
@@ -14,6 +14,13 @@ class MainPage(FormView):
     template_name = 'home.html'
     form_class = ContactForm
 
+    def post(self, request):
+        form = request.POST
+        message = u''
+        for value in form.values():
+            message += (value + u' ')
+        sm('test message', message, 'test@mail.com', [ADMIN_EMAIL])
+        return HttpResponseRedirect(reverse('home'))
 
 def send_mail(request):
     if request.method == 'POST':
